f86b0784df215e6e0959ffcadd6dae11509fbe89
AH-dark <ahdark@outlook.com>
2022-01-01 18:22:16 +0800

    Fix a bug in makeStyles()

--- components/StatusCard/index.tsx
+++ components/StatusCard/index.tsx
@@ -23,12 +23,7 @@ const useStyles = makeStyles((theme: Theme) =>
             width: "100%",
             display: "flex",
             flexDirection: "column",
-            alignItems: "flex-start",
-            "last-of-type": {
-                border: 0,
-                marginBottom: 0,
-                paddingBottom: 0,
-            },
+            alignItems: "flex-start"
         },
         header: {
             display: "flex",
